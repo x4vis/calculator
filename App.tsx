b9b694ab5e2fb38869432ae6c9de63022b61a88a
Javier Soto <javislop@gmail.com>
2021-04-21 17:06:30 -0400

    init project

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { SafeAreaView } from 'react-native'
+import CalculadoraScreen from './src/screens/CalculadoraScreen'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <CalculadoraScreen />
+    </SafeAreaView>
+  )
+}
+
+export default App
